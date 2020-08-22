75486966f25046da1bbe6f4c04d899748e96b157
Rakesh Bandi <rbandi@ucsc.edu>
2020-08-21 17:12:10 -0700

    Ch 1 Complete

--- CTCI/Ch_1/1.8/Solution.java
+++ CTCI/Ch_1/1.8/Solution.java
@@ -3,17 +3,15 @@ import java.util.*;
 class Solution{
 
    static boolean isRotation(char[] str, char[] str2){
-      if(str==null||str2==null||str.length!=str2.length) return false;
-      if(str.length==0) return true;
+      if(str==null||str2==null||str.length!=str2.length||str.length==0) return false;
       char[] poss_Rotations = new char[str.length*2];
       for(int i=0;i<str.length;++i) poss_Rotations[i] = str[i];
       for(int i=0;i<str.length;++i) poss_Rotations[str.length+i] = str[i];
-      if(isSubstring(str2,poss_Rotations)) return true;
-      else return false;
+      return isSubstring(str2,poss_Rotations));
    }
 
    public static void main(String args[]){
-      System.out.println("Can't check this, because we don't have code for substring()");
+      System.out.println("*** Need code for substring() to test ***");
       if(isRotation({'a','b','c'},{'c','a','b'})) System.out.println("Success!");
       else System.out.println("Fail.");
    }
