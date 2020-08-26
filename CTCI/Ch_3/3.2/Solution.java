6820f934d61e830f996af4dc32a1187b853aa75e
Rakesh Bandi <rbandi@ucsc.edu>
2020-08-26 13:29:02 -0700

    3.4

--- CTCI/Ch_3/3.2/Solution.java
+++ CTCI/Ch_3/3.2/Solution.java
@@ -1,24 +1,62 @@
 import java.util.*;
 
 class Solution{
-
-   static void function(/*params*/){
-      //process
+   /*How would you design a stack which, in addition to push and pop, also has a function
+   min which returns the minimum element? Push, pop and min should all operate in
+   O(1) time*/
+   static class Node{
+      Node next;
+      int val;
+      int min;
+      Node(int k){
+         next = null;
+         val = k;
+         min = k;
+      }
+   }
+   static class Stack{
+      //can also be implemented as linked list w/ buffer, nodeptr replaced w/ cur
+      Node[] buffer;
+      int nodePtr;
+      Stack(int len){
+         buffer = new Node[len];
+         nodePtr = -1;
+      }
+      void push(int k){
+         if(nodePtr==buffer.length-1){
+            System.out.println("Err: stack full");
+            return;
+         }
+         Node t = new Node(k);
+         if(this.peek().min<t.min) t.min = this.peek().min;
+         buffer[++nodePtr] = t;;
+      }
+      Node pop(){
+         if(nodePtr==-1){
+            System.out.println("Err: Stack Empty");
+            return null;
+         }
+         Node ret = this.peek();
+         buffer[nodePtr--] = null;
+         return ret;
+      }
+      Node peek(){
+         if(nodePtr==-1){
+            System.out.println("Err: Stack Empty");
+            return null;
+         }
+         return buffer[nodePtr];
+      }
+      boolean isEmpty(){
+         return nodePtr == -1;
+      }
+      int min(){
+         if(this.isEmpty()) return Integer.MAX_VALUE;
+         else return this.peek().min;
+      }
    }
 
    public static void main(String args[]){
-      HashMap</*input*/,/*output*/> mapping = new HashMap</*input*/,/*output*/>();
-
-      String b = null;
-      //mapping.put(/*input*/, /*predicted output*/);
-      //mapping.put(...), etc.
-
-      int pass = 0; /*pass2, pass3, etc. */
-      int tests = mapping.size();
-      for(/*inputType input*/ : mapping.keySet()){
-         if(mapping.get(/*input*/)==function(s)) pass++;
-      }
-
-      System.out.println("function: "+pass+"/"+tests+" passed.");
+      //no testing for this..
    }
 }
--- CTCI/Ch_3/3.4/Solution.java
+++ CTCI/Ch_3/3.4/Solution.java
@@ -1,24 +1,34 @@
 import java.util.*;
 
 class Solution{
-
-   static void function(/*params*/){
-      //process
+   static class Stack{/* Assume it works...*/
+      int k;
+   }
+   static void hanoi(Stack[] stacks, int elems){
+      if(elems<1){
+         System.out.println("Err: elems: "+elems+". must be greater than 0.");
+         return;
+      }
+      stacks_initialize(stacks, elems);
+      if(elems==1) stacks[2].push(stacks[0].pop());
+      else hanoi(0,elems,2);
+   }
+   static void hanoi_rec(int source, int elems, int target){
+      if(elems<1) return;
+      if(elems==1){
+         stacks[target].push(stacks[source].pop());
+         return;
+      }
+      int empty;
+      if(source!=0&&target!=0) empty = 0;
+      if(source!=1&&target!=1) empty = 1;
+      if(source!=2&&target!=2) empty = 2;
+      hanoi_rec(source,elems-1,empty);
+      stacks[target].push(stacks[source].pop());
+      hanoi_rec(empty,elems-1,target);
    }
 
    public static void main(String args[]){
-      HashMap</*input*/,/*output*/> mapping = new HashMap</*input*/,/*output*/>();
-
-      String b = null;
-      //mapping.put(/*input*/, /*predicted output*/);
-      //mapping.put(...), etc.
-
-      int pass = 0; /*pass2, pass3, etc. */
-      int tests = mapping.size();
-      for(/*inputType input*/ : mapping.keySet()){
-         if(mapping.get(/*input*/)==function(s)) pass++;
-      }
-
-      System.out.println("function: "+pass+"/"+tests+" passed.");
+      // no testing here..
    }
 }
