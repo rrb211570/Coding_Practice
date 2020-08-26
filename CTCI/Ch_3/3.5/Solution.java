e08548a25ae3e086da5b413330378b6a48db5364
Rakesh Bandi <rbandi@ucsc.edu>
2020-08-26 13:44:32 -0700

    Ch 3 Complete

--- CTCI/Ch_3/3.5/Solution.java
+++ CTCI/Ch_3/3.5/Solution.java
@@ -2,23 +2,39 @@ import java.util.*;
 
 class Solution{
 
-   static void function(/*params*/){
-      //process
+   static class MyQueue{
+      Stack<Integer> in;
+      Stack<Integer> out;
+      MyQueue(){
+         in = new Stack<Integer>();
+         out = new Stack<Integer>();
+      }
+      static void add(int k){
+         in.push(k);
+         if(out.isEmpty()){
+            while(!in.isEmpty()){
+               out.push(in.pop());
+            }
+         }
+      }
+      static int remove(){
+         if(out.isEmpty()){
+            System.out.println("Err: MyQueue is empty.");
+            return -1;
+         } else return out.pop();
+      }
+      static int peek(){
+         if(out.isEmpty()){
+            System.out.println("Err: MyQueue is empty.");
+            return -1;
+         } else return out.peek();
+      }
+      static int length(){
+         return in.length() + out.length();
+      }
    }
 
    public static void main(String args[]){
-      HashMap</*input*/,/*output*/> mapping = new HashMap</*input*/,/*output*/>();
-
-      String b = null;
-      //mapping.put(/*input*/, /*predicted output*/);
-      //mapping.put(...), etc.
-
-      int pass = 0; /*pass2, pass3, etc. */
-      int tests = mapping.size();
-      for(/*inputType input*/ : mapping.keySet()){
-         if(mapping.get(/*input*/)==function(s)) pass++;
-      }
-
-      System.out.println("function: "+pass+"/"+tests+" passed.");
+      // no tests here...
    }
 }
