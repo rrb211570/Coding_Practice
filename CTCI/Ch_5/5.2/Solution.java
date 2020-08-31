9436d4a1bdf3ea6934d737377bdfdfaac490585e
Rakesh Bandi <rbandi@ucsc.edu>
2020-08-30 20:40:39 -0700

    5.2

--- CTCI/Ch_5/5.2/Solution.java
+++ CTCI/Ch_5/5.2/Solution.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
 class Solution{
-
-   static void function(/*params*/){
-      //process
+   //Assume val correctly contains a double
+   static void printDecToBin(String val){
+      try{
+         if(val==null) throw new NullPointerException();
+         String preDecimal;
+         String postDecimal;
+         int index=-1;
+         while(val.charAt(++index)!='.');
+         preDecimal = Integer.toBinaryString(Integer.parseInt(val.substring(0,index)));
+         postDecimal = Integer.toBinaryString(Integer.parseInt(val.substring(index+1,val.length())));
+         System.out.println(preDecimal+"."+postDecimal);
+      } catch(Exception e){
+         System.out.println("ERROR: printDecToBin(): "+e);
+      }
    }
 
-   public static void main(String args[]){
-      HashMap</*input*/,/*output*/> mapping = new HashMap</*input*/,/*output*/>();
-
-      String b = null;
-      //mapping.put(/*input*/, /*predicted output*/);
-      //mapping.put(...), etc.
-
-      int pass = 0; /*pass2, pass3, etc. */
-      int tests = mapping.size();
-      for(/*inputType input*/ : mapping.keySet()){
-         if(mapping.get(/*input*/)==function(s)) pass++;
-      }
-
-      System.out.println("function: "+pass+"/"+tests+" passed.");
+   public static void main(String args[]){]
+      printDecToBin("0.101");
+      printDecToBin("13.111");
    }
 }
